03fa0cbe3461ea7c98f1f109cb9a38320d8f3b7e
Andres Martinez <andresgmartinezb@gmail.com>
2017-07-21 12:56:30 -0400

    Ordenando carpetas

--- basica/test/setup.py
+++ basica/test/setup.py
@@ -1,4 +1,7 @@
 # Archivo de prueba
+import sys
+sys.path.append('')
+
 from basica.basica import basica
 
 valor = basica(5)
--- otra/test/setup.py
+++ otra/test/setup.py
@@ -1,4 +1,7 @@
 # Archivo de prueba
+import sys
+sys.path.append('')
+
 from otra.otra import otra
 
 valor = otra(5)
